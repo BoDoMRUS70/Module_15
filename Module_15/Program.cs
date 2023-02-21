1bdd7d58497f14b157b05f6803658907be3f6b67
Bodomrus70 <Bodomrus70@mail.ru>
2023-02-21 22:01:33 +0700

    Добавьте файлы проекта.

---
+++ Module_15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Module_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var names = from s in classes select s.Students;
+            foreach (var name in names)
+            {
+                sb.Append(string.Join(" ", name.ToArray()));
+                sb.Append(" ");
+            }
+
+            return sb.ToString().Split(Environment.NewLine.ToCharArray());
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
